a0cfc933f05e07b8aa30a05e3f2aecde3cb4c841
marietha.asnat <marietha.asnat@ui.ac.id>
2022-09-14 11:06:10 +0700

    Upload Tugas 2 PBP

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,14 @@
+# Nama : Marietha Asnat Nauli Sitompul
+# NPM : 2106752413
+# Kelas : PBP - B
+# Kode Asdos : BI
+
+from django.urls import path
+from katalog.views import katalog_view
+
 # TODO: Implement Routings Here
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', katalog_view, name = 'katalog_view'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# Nama : Marietha Asnat Nauli Sitompul
+# NPM : 2106752413
+# Kelas : PBP - B
+# Kode Asdos : BI
 
-# TODO: Create your views here.
+from django.shortcuts import render
+from katalog.models import CatalogItem
+
+# TODO : Create your views here.
+def katalog_view (request):
+    katalog_item = CatalogItem.objects.all()
+    context = {
+    'list_item': katalog_item,
+    'Nama': 'Marietha Asnat Nauli Sitompul',
+    'ID': '2106752413'
+    }
+    return render(request, "katalog.html", context)
