12f4313797c7b88e00b54ecd7435b1ab9a010295
marietha.asnat <marietha.asnat@ui.ac.id>
2022-09-21 18:02:41 +0700

    Update Tugas 3 PBP

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from unicodedata import decimal
 
 class WatchlistItem(models.Model):
     watched = models.BooleanField()
     title = models.CharField(max_length = 25)
-    # rating = models.DecimalField(max_digits = 5, decimal_places = 1)
     rating = models.FloatField()
     release_date = models.DateField()
     review = models.TextField()
